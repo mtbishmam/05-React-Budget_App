916a7d6657aa476129b14adf4f9bc88c12114a8e
mtbishmam <mtbishmam@gmail.com>
2024-05-31 12:54:58 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>Hello</>;
+}
+
+export default App;
